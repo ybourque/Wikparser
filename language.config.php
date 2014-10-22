af749d65197fb893290736c5643c9ef4004ec0e9
ybourque <ybourque@gmail.com>
2014-10-22 18:06:40 -0400

    Update to language parameters
    New parameters added; support for other languages added.

--- language.config.php
+++ language.config.php
@@ -1,67 +1,118 @@
 <?php
-/////////////////////////////////////////////////////////////////////////////////////
+/***********************************************************************************/
 // Add a case for any additional languages and set variables according to the strings
 // used by Wiktionary for the given language. See www.igrec.ca/projects/ for more
 // info on how to do this.
-/////////////////////////////////////////////////////////////////////////////////////
+// Basics:
+/*case "2-letter language code":
+		$this->langCode = 2-letter language code
+		$this->langHeader = string that begins section for a given language
+		$this->langSeparator = string that indicates a new language section
+		$this->defHeader = header for definitions section; may not be present
+		$this->defTag = string that begins a definition
+		$this->synHeader = header for synonyms
+		$this->hyperHeader = header for hypernyms
+		$this->genderPattern = regex for gender
+		$this->posExtraString = extra string around POS that can be stripped
+		$this->posPattern = regex pattern for POS
+		$this->posArray = array(all possible values for POS); leave empty if using preg
+		$this->posMatchType = 'preg' for regular expressions; 'array' for array */
+/***********************************************************************************/
 
-switch ($langcode) {
+switch ($this->langCode) {
 // English parameters	
 	case "en":
-		$this->langcode = "en";
-		$this->langheader = "==English==";
-		$this->langseparator = "----";
-		$this->deftag = "# ";
-		$this->synheader = "====Synonyms====";
-		$this->hyperheader = "====Hypernyms====";
-		$this->posextrastring = "=";
-		$this->posmatchtype = "array";
-		$this->posarray = array(
+		$this->langCode = "en";
+		$this->langHeader = "==English==";
+		$this->langSeparator = "----";
+		$this->defHeader = "";
+		$this->defTag = "# ";
+		$this->synHeader = "====Synonyms====";
+		$this->hyperHeader = "====Hypernyms====";
+		$this->genderPattern = "";
+		$this->posMatchType = "array";
+		$this->posArray = array(
 			"===Noun===", "===Verb===", "===Adjective===", "===Adverb===", "===Preposition===",
 			"===Particle===", "===Pronouns===", "===Interjection===", "===Conjunction===",
 			"===Article===");
+		$this->posExtraString = "=";
 		break;
 // French parameters
 	case "fr":
-		$this->langcode = "fr";
-		$this->langheader = "fr}} ==";
-		$this->langseparator = "== {{=";
-		$this->deftag = "# ";
-		$this->hyperheader = "{{-syn-}}";
-		$this->synheader = "{{-hyper-}}";
-		$this->posmatchtype = "preg";
-	// Old Regex. Wiktionary changed French POS. Updated here 8 Feb 2014.
-//		$this->pospattern = "(\{\{\-[a-zA-ZéÉèÈàÀ]+\-[a-zA-ZéÉèÈàÀ]*\-?(\|num=\d)?\|fr(\|num=\d)?\}\})";
-		$this->pospattern = "(\{\{\S\|[\d\w\s]+\|fr(\|num=[0-9])?\}\})u";
+		$this->langCode = "fr";
+		$this->langHeader = "fr}} ==";
+		$this->langSeparator = "== {{=";
+		$this->defHeader = "";
+		$this->defTag = "# ";
+		$this->synHeader = "==== {{S|synonymes}} ====";
+		$this->hyperHeader = "==== {{S|hyperonymes}} ====";
+		$this->genderPattern = "(\{\{([mf]|mf)\??\}\})";
+		$this->posMatchType = "preg";
+		$this->posPattern = "(\{\{\S\|[\d\w\s]+\|fr(\|num=[0-9])?\}\})u";
+		$this->posArray = array();
+		$this->posExtraString = "{{S|";
+		break;
+// Spanish parameters
+	case "es":
+		$this->langCode = "es";
+		$this->langHeader = "{{ES";
+		$this->langSeparator = "";
+		$this->defHeader = "";
+		$this->defTag = ";";
+		$this->synHeader = "'''Sinónimo";
+		$this->hyperHeader = "";
+		$this->genderPattern = "(\s?(masculino|femenino)(\|es)?\}\}\s?===)";
+		$this->posMatchType = "preg";
+		$this->posPattern = "(===\s?\{\{\w*[\|\s])u";
+		$this->posArray = array();
+		$this->posExtraString = "";
+		break;
+// German parameters
+	case "de":
+		$this->langCode = "de";
+		$this->langHeader = "Deutsch}}) ==";
+		$this->langSeparator = "({{Sprache|";
+		$this->defHeader = "{{Bedeutungen}}";
+		$this->defTag = ":";
+		$this->synHeader = "{{Synonyme}}";
+		$this->hyperHeader = "{{Oberbegriffe}}";
+		$this->genderPattern = "(\{\{[mfn]\}\}\s===)";
+		$this->posMatchType = "preg";
+		$this->posPattern = "(\{\{Wortart\|\w+\|)";
+		$this->posArray = array();
+		$this->posExtraString = "{{Wortart|";
 		break;
 // Fill in the following settings for a language of your choice.
 	case "":
-		$this->langcode = "";
-		$this->langheader = "";
-		$this->langseparator = "";
-		$this->deftag = "";
-		$this->synheader = "";
-		$this->hyperheader = "";
-		$this->posextrastring = "";
-		$this->posmatchtype = "";
-		$this->pospattern = "";
-		$this->posarray = array();
+		$this->langCode = "";		// string
+		$this->langHeader = "";		// string
+		$this->langSeparator = "";	// string
+		$this->defHeader = "";		// string
+		$this->defTag = "";			// string
+		$this->synHeader = "";		// string
+		$this->hyperHeader = "";	// string
+		$this->genderPattern = "";	// regular expression
+		$this->posExtraString = "";	// string
+		$this->posMatchType = "";	// 'preg' or 'array'
+		$this->posPattern = "";		// regular expression
+		$this->posArray = array();	// array
 		break;		
 // Default parameters (currently english)		
 	default:
-		$this->langcode = "en";
-		$this->langheader = "==English==";
-		$this->langseparator = "----";
-		$this->deftag = "# ";
-		$this->synheader = "====Synonyms====";
-		$this->hyperheader = "====Hypernyms====";
-		$this->posextrastring = "=";
-		$this->posmatchtype = "array";
-		$this->posarray = array(
+		$this->langCode = "en";
+		$this->langHeader = "==English==";
+		$this->langSeparator = "----";
+		$this->defHeader = "";
+		$this->defTag = "# ";
+		$this->synHeader = "====Synonyms====";
+		$this->hyperHeader = "====Hypernyms====";
+		$this->genderPattern = "";
+		$this->posExtraString = "=";
+		$this->posMatchType = "array";
+		$this->posArray = array(
 			"===Noun===", "===Verb===", "===Adjective===", "===Adverb===", "===Preposition===",
 			"===Particle===", "===Pronouns===", "===Interjection===", "===Conjunction===",
 			"===Article===");
 		break;
 }
-
 ?>
